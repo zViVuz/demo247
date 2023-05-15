e7013a9d5da4ff724f18b0bc26563e0e3862eca3
vinhnq <0904782309c.gmail.com>
2023-05-15 23:08:39 +0700

    Ex-6-7

---
+++ webreactjs247/src/shared/ListOfPlayers.js
@@ -0,0 +1,8 @@
+export const Players =[
+    {id: 1, name: 'Cristiano Ronaldo', club: 'Manchester United', img:'assets/images/cr.jpg'},
+    {id: 2, name: 'Kante', club: 'Chelsea', img:'assets/images/kante.jpg'},
+    {id: 2, name: 'Messi', club: 'PSG', img:'assets/images/messi.jpg'},
+    {id: 2, name: 'Neymar', club: 'PSG',img:'assets/images/neymar.jpg'},
+    {id: 2, name: 'Kane', club: 'Tottemham',img:'assets/images/kane.jpg' },
+    {id: 2, name: 'Haaland', club: 'Manchester City', img:'assets/images/haaland.jpg'}
+  ];
