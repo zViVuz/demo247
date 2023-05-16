6444dc6ca6a909aa6fbfec6d0d82b779404fa9e0
vinhnq <0904782309c.gmail.com>
2023-05-16 13:34:59 +0700

    Functional Components

--- webreactjs247/src/components/Navigation.js
+++ webreactjs247/src/components/Navigation.js
@@ -1,18 +1,15 @@
-import React, { Component } from "react";
-export default class Navigation extends Component {
-  render() {
-    return (
-        <div className="navigation">
-            <nav>
-                <ul>
-                    <li><a className="active" href="#home">Home</a></li>
-                    <li><a href="#news">News</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#Contact">Contact</a></li>
-                </ul>
-            </nav>
-        </div>
-        
-    );
-  }
+import React from 'react'
+export default function Navigation() {
+  return (
+    <div className='navigation'>
+      <nav>
+        <ul>
+          <li><a className='active' href='#home'>Home</a></li>
+          <li><a href='#news'>News</a></li>
+          <li><a href='#about'>About</a></li>
+          <li><a href='#contact'>Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  )
 }
